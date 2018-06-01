caa960b0f6b0238e5204c375bfe44d55315cc030
puresoda <35409575+puresoda@users.noreply.github.com>
2018-06-01 12:10:39 -0700

    Kaleb's Tuning and Cleanup

--- Capstone.ino
+++ Capstone.ino
@@ -1,3 +1,15 @@
+//Battery2
+  //---
+  //7.2 V (Much later)
+
+//Battery3
+  //9.6 V
+  //8.3 V (1.5 hrs)
+
+//Battery4
+  //9.68 V
+  //8.6 (30 min) (ideal!) (as in, we gucci)
+
 //IR Sensors
 #define L_SENSOR 19
 #define F_SENSOR 17
@@ -13,18 +25,29 @@
 #define R_WHEEL 5
 #define L_WHEEL 6
 
-double K_P = 1.5;  //for PD
-double K_P_R = 5;
-double K_D = 0.60;
+//Best: K_P = 0.4, K_P_R = 10.5, K_D = 2, K_D_R = 2
+//New Best: K_P = 0.4, K_P_R = 12.5, K_D = 9, K_D_R = 17
+double K_P = 0.40;  //for PD
+double K_P_R = 12.5;
+double K_D = 9;
+double K_D_R = 17;
 int L_prevError = 0;
 int R_prevError = 0;
 
-double L_zero = 0;  //calibration
-double F_zero = 0;
-double R_zero = 0;
+long L_zero = 0;  //calibration
+long F_zero = 0;
+long R_zero = 0;
 
-int L_BASE = 90; //base speeds
-int R_BASE = 90;
+int L_BASE = 215; //base speeds
+int R_BASE = 215;
+
+//previously moving output
+//int R_prev = R_BASE;
+//int L_prev = L_BASE;
+
+//turning corrections
+double L_correction = 1.5;
+double R_correction = 2.5;
 
 const bool debug = false;
 
@@ -54,6 +77,7 @@ void setup()
 
 void loop() 
 {  
+  //turn();
   //gottaGoFast();  //hack into the matrix
   int L_READ = analogRead(L_SENSOR);
   int R_READ = analogRead(R_SENSOR);
@@ -61,21 +85,46 @@ void loop()
   //Measure error
   int L_error = L_READ - L_zero;
   int R_error = R_READ - R_zero;
-
+  
   //PID control
-  int L_output = L_BASE + K_P * L_error - K_D * (L_error - L_prevError);
-  int R_output = R_BASE + K_P_R * R_error - K_D * (R_error - R_prevError); 
+  int L_output = L_BASE + K_P * L_error + K_D * (L_error - L_prevError);
+  int R_output = R_BASE + K_P_R * R_error + K_D_R * (R_error - R_prevError); 
 
   L_output = constrain(L_output, 100, 255);
   R_output = constrain(R_output, 100, 255);
+  
+  //checking for emptiness lol
+  if (L_READ < 600)
+  {
+    analogWrite(L_WHEEL, L_output + L_correction * K_D * (L_error - L_prevError));
+    analogWrite(R_WHEEL, R_output);
+  }
+  else if (R_READ < 600)
+  {
+    analogWrite(L_WHEEL, L_output);
+    analogWrite(R_WHEEL, R_output + R_correction * K_D_R * (R_error - R_prevError));
+  }
+  else
+  {
+    analogWrite(L_WHEEL, L_output);
+    analogWrite(R_WHEEL, R_output);
+  }
 
   L_prevError = L_error;
   R_prevError = R_error;
 
+  //L_prev = L_output;
+  //R_prev = R_output;
+
+//  Serial.println(String(L_READ) + " - " + String(L_zero) + " = " + String(L_error));
+//  Serial.println(String(K_P * L_error) + " - " + String(K_D * (L_error - L_prevError)) + " = " + String(L_output));
+//  Serial.println(String(R_READ) + " - " + String(R_zero) + " = " + String(R_error));
+//  Serial.println(String(K_P_R * R_error) + " - " + String(K_D_R * (R_error - R_prevError)) + " = " + String(R_output) + '\n');
+  
   if(debug)
   {
     Serial.println("Left proportional: " + String(K_P * L_error));
-    Serial.println("Right proportional: " + String(K_P * R_error));
+    Serial.println("Right proportional: " + String(K_P_R * R_error));
     Serial.println("=========================");
     Serial.println("Left derivative: " + String(K_D * (L_error - L_prevError)));
     Serial.println("Right derivative: " + String(K_D * (R_error - R_prevError)));
@@ -86,21 +135,37 @@ void loop()
     Serial.println("Right read: " + String(R_READ));
     Serial.println("*************************");
   }
-  
-  analogWrite(L_WHEEL, L_output);
-  analogWrite(R_WHEEL, R_output);
 
-  if(analogRead(F_SENSOR) > 900)  //only turns if the front is blocked
+  if(analogRead(F_SENSOR) > 830)  //only turns if the front is blocked
   { 
+    //fast stop
+    digitalWrite(L_1, HIGH);  
+    digitalWrite(L_2, HIGH);
+    digitalWrite(R_1, HIGH);
+    digitalWrite(R_2, HIGH);
+    
+    analogWrite(L_WHEEL, 255);
+    analogWrite(R_WHEEL, 255);
+
+    delay(300);
+
     analogWrite(L_WHEEL, 0);
     analogWrite(R_WHEEL, 0);
 
-    delay(300);
+    digitalWrite(L_1, LOW);  
+    digitalWrite(L_2, HIGH);
+    digitalWrite(R_1, HIGH);
+    digitalWrite(R_2, LOW);
     
-    if(analogRead(R_SENSOR) > 200)
+    if(L_READ < 350)
       left();
-    else if(analogRead(L_SENSOR) > 200)
+    else
       right();
+
+    digitalWrite(L_1, LOW);  
+    digitalWrite(L_2, HIGH);
+    digitalWrite(R_1, HIGH);
+    digitalWrite(R_2, LOW);
 
     delay(300);
   }
@@ -134,7 +199,8 @@ void left() //90 degree turn
   analogWrite(L_WHEEL, 150);
   analogWrite(R_WHEEL, 150);
 
-  delay(295);
+  //delay(295);
+  delay(225);
 
   digitalWrite(R_1, HIGH);
   digitalWrite(R_2, LOW);
@@ -151,7 +217,8 @@ void right()  //90 degree turn
   analogWrite(L_WHEEL, 150);
   analogWrite(R_WHEEL, 150);
 
-  delay(295);
+  //delay(295);
+  delay(225);
   
   digitalWrite(L_1, LOW);
   digitalWrite(L_2, HIGH);
